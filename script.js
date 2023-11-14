9315f7d6aa5dc1cc921b7a0fc4149a51abb6ee15
gracebell29 <111467576+gracebell29@users.noreply.github.com>
2023-11-14 12:53:09 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Map Gallery</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        #gallery {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+            gap: 20px;
+        }
+
+        .map {
+            border: 1px solid #ccc;
+            padding: 10px;
+            text-align: center;
+        }
+
+        img {
+            max-width: 100%;
+            height: auto;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            margin-bottom: 8px;
+        }
+    </style>
+</head>
+<body>
+
+<div id="gallery"></div>
+
+<script>
+    // Sample data - replace with your own data
+    const mapData = [
+        { url: 'map1.jpg', description: 'Description for Map 1' },
+        { url: 'map2.jpg', description: 'Description for Map 2' },
+        { url: 'map3.jpg', description: 'Description for Map 3' },
+        // Add more images as needed
+    ];
+
+    // Function to create map elements
+    function createMapElement(map) {
+        const mapElement = document.createElement('div');
+        mapElement.classList.add('map');
+
+        const image = document.createElement('img');
+        image.src = map.url;
+        mapElement.appendChild(image);
+
+        const description = document.createElement('p');
+        description.textContent = map.description;
+        mapElement.appendChild(description);
+
+        return mapElement;
+    }
+
+    // Populate the gallery with map elements
+    const gallery = document.getElementById('gallery');
+    mapData.forEach(map => {
+        const mapElement = createMapElement(map);
+        gallery.appendChild(mapElement);
+    });
+</script>
+
+</body>
+</html>
